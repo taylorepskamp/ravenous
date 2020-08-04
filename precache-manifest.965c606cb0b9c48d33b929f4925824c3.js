749ac5c1fb6f8635951f7906493bf2771630d41d
Taylor Epskamp <taylorepskamp@gmail.com>
2020-08-04 14:31:02 +0200

    Updates

---
+++ precache-manifest.965c606cb0b9c48d33b929f4925824c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f37db3eaa19036c43c5f368c0e43fd6",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "06d726783f1c1e91d1fe",
+    "url": "/ravenous/static/css/main.18bd43f2.chunk.css"
+  },
+  {
+    "revision": "4c46fcc5e34cc2b94508",
+    "url": "/ravenous/static/js/2.dbfe3253.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ravenous/static/js/2.dbfe3253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06d726783f1c1e91d1fe",
+    "url": "/ravenous/static/js/main.5286f588.chunk.js"
+  },
+  {
+    "revision": "743ea5a2f8f5a871598b",
+    "url": "/ravenous/static/js/runtime-main.faf31fb8.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
